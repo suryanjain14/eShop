05978eed1da5f9cdd73f4ef91932b29652261e79
Suryan Jain <suryanjain14@gmail.com>
2020-08-14 23:52:18 +0530

    contact form partially completed form validation not working

--- contactus/views.py
+++ contactus/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect,HttpResponse
+from .forms import ContactForm
 
 # Create your views here.
 
 def contact(request):
-    return render(request, 'contactus/contact.html', {})
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+           return HttpResponse("<h1>Check your form </h1>")
+    else:
+        form = ContactForm()
+        return render(request, 'contactus/contact.html', {'form':form})
