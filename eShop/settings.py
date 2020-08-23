9c5661a58be27e77813bc9173e21306cbd4db7c8
Suryan Jain <suryanjain14@gmail.com>
2020-08-23 20:33:40 +0530

    debug True

--- eShop/settings.py
+++ eShop/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/assets')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
