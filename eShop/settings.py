350b3c2dac02d1e4405c85166109c351101f4cee
Suryan Jain <suryanjain14@gmail.com>
2020-08-23 17:06:24 +0530

    final cut

--- eShop/settings.py
+++ eShop/settings.py
@@ -117,8 +117,10 @@ USE_TZ = True
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/assets')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
